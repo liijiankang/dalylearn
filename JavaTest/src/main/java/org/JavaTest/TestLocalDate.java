bed818f8dd25e1c406512eac215d3313afa5e14e
ljk <lijk_007@163.com>
2018-11-02 10:05:47 +0800

    java8的新用法

---
+++ JavaTest/src/main/java/org/JavaTest/TestLocalDate.java
@@ -0,0 +1,11 @@
+package org.JavaTest;
+
+import java.time.LocalDate;
+
+public class TestLocalDate {
+
+	public static void main(String[] args) {
+		LocalDate nowTime = LocalDate.now();
+		System.out.println(nowTime);
+	}
+}
